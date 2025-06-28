4440ba6ca7520114f301835341db164bf511385a
blerfx2 <blerfx@byu.edu>
2025-06-27 22:31:10 -0600

    Update

--- NordicSpellCrafting/Classifications.py
+++ NordicSpellCrafting/Classifications.py
@@ -5,63 +5,86 @@ import pixie
         3. Make the components for the spell components (i.e. lines, curves, semicircles, etc.)
         4. Figure out how you want to mush the data together
         """
+paint = pixie.Paint(pixie.SOLID_PAINT)
+paint.color = pixie.parse_color("#000000")
 
-class Type:
-    def __init__(spell):
-        pass
+
+def smallCurve(image, x, y):
+    path = pixie.parse_path(
+    """
+    M 900 200
+    A 100 100 90 0 0 1100 200
+    """
+    )
+    image.stroke_path(path, paint, stroke_width=20)
+        
+def bigCurve(image, x, y):
+    path = pixie.parse_path(
+        """
+        M 1150 200
+        A 150 150 90 0 1 850 200
+        """
+    )
+    image.stroke_path(path, paint, stroke_width=20)
+
+def mainLine(ctx, x, y):
+    ctx.stroke_segment(x, y, 1000, 1000)
+
+def line(ctx, x1, y1, x2, y2):
+    ctx.stroke_segment(905, 500, 1095, 500)
+
+def strSave(ctx):
+    ctx.stroke_rect(900, 900, 200, 200)
+    ctx.stroke_rect(950, 950, 100, 100)
+     
+
+
+class Identity:
+    def __init__(spell, name):
+        spell.name = name
+        spell.damage = 0
+        spell.x = 1000 # x and y are more like the center of where to put any symbol
+        spell.y = 275
 
 
 
 def main():
     image = pixie.Image(2000,2000)
-    image.fill(pixie.Color(75, 5, 5, 1))
-
-    paint = pixie.Paint(pixie.SOLID_PAINT)
-    paint.color = pixie.parse_color("#000000")
+    image.fill(pixie.Color(1, 1, 1, 1))
 
     ctx = image.new_context()
     ctx.stroke_style = paint
     ctx.line_width = 25
+    mainLine(ctx)
+    bigCurve(image)
 
-    ctx.stroke_segment(1000, 200, 1000, 1000)
 
-    spath = pixie.parse_path(
-        """
-        M 900 200
-        A 100 100 90 0 0 1100 200
-        M 1150 200
-        A 150 150 90 0 1 850 200
-        """
-    )
     ctx.line_width = 20
-    ctx.stroke_segment(905, 500, 1095, 500)
-    ctx.stroke_segment(905, 700, 1095, 700)
-    ctx.stroke_rect(900, 900, 200, 200)
-    ctx.stroke_rect(950, 950, 100, 100)
+    ctx.stroke_segment(920, 290, 1080, 220)
+    ctx.stroke_segment(920, 220, 1080, 300)
 
-    image.stroke_path(spath, paint, stroke_width=20)
+
+
+    image.write_file("NordicSpellCrafting/trying.png")
+
 
     # Sideways
 
-    ctx.stroke_segment(1000, 1000, 1800, 1000)
+    # ctx.stroke_segment(1000, 1000, 1800, 1000)
 
-    spath = pixie.parse_path(
-        """
-        M 1800 900
-        A 100 100 0 0 0 1800 1100
-        M 1800 1150
-        A 150 150 0 0 1 1800 850
-        """
-    )
-    ctx.line_width = 20
-    ctx.stroke_segment(1500, 905, 1500, 1095)
-    ctx.stroke_segment(1300, 905, 1300, 1095)
-    image.stroke_path(spath, paint, stroke_width=20)
-    # cimage = image.copy()
-    # cimage.flip_vertical()
-    # cimage = cimage.sub_image(0,1000,2000,1000)
-    # image.draw(cimage)
-    image.write_file("trying.png")
+    # spath = pixie.parse_path(
+    #     """
+    #     M 1800 900
+    #     A 100 100 0 0 0 1800 1100
+    #     M 1800 1150
+    #     A 150 150 0 0 1 1800 850
+    #     """
+    # )
+    # ctx.line_width = 20
+    # ctx.stroke_segment(1500, 905, 1500, 1095)
+    # ctx.stroke_segment(1300, 905, 1300, 1095)
+    # image.stroke_path(spath, paint, stroke_width=20)
+
 
 
 if __name__ == "__main__":
