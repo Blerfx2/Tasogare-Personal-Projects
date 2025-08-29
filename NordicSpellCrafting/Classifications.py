f7b13c1c1c0143112e062c8c147e4f5e028e6f04
Blerfx2 <blerfx@byu.edu>
2025-08-28 20:14:53 -0600

    Almost done!

--- NordicSpellCrafting/Classifications.py
+++ NordicSpellCrafting/Classifications.py
@@ -1,8 +1,11 @@
 import pixie
 
-"""TODO 1. In the spell class, make a function that takes the x and y values of each piece and does the correct calculation to rotate in the correct directions
-        1.5 Might need to make a half rectangle function for things like 120 ft.
-        2. Make a second function that does the same thing for diagonals
+angle_degrees = 45
+angle_radians = angle_degrees * (3.14159 / 180)
+
+
+"""TODO 1. Make each draw function do a for loop with a range. Modify all functions to account for all directions
+        2 Might need to make a half rectangle function for things like 120 ft.
         3. For the Deception, Shapechanging, and Detection stave heads would need an if statement to change line length.
         4. Make the components for the spell components (i.e. lines, curves, semicircles, etc.)
         5. Figure out how you want to mush the data together
@@ -14,157 +17,269 @@ fill = pixie.Paint(pixie.SOLID_PAINT)
 fill.color = pixie.parse_color("#FFFFFF")
 
 
-def smallCurve(image, x, y, flip=0):
+def smallCurve(image, y, rotation=0):
+# y should normally be the top of the segment
+    if rotation in [1,3]:
+        y = 2000 - y
     path = pixie.parse_path(
     f"""
-    M 900 200
-    A 100 100 90 0 0 1100 200
+    M 900 {y}
+    A 100 100 90 0 {rotation} 1100 {y}
     """
     )
     image.stroke_path(path, paint, stroke_width=20)
         
-def bigCurve(image): # Stave only
+def bigCurve(image, rotation = 0): # Stave only
+    y = 200
+    if rotation in [1,3]:
+        y = 2000 - y
+        rotation = 0
+    else:
+         rotation = 1
     path = pixie.parse_path(
-        """
-        M 1150 200
-        A 150 150 90 0 1 850 200
+        f"""
+        M 1150 {y}
+        A 150 150 90 0 {rotation} 850 {y}
         """
     )
     image.stroke_path(path, paint, stroke_width=20)
 
-def halfRec(ctx, flip=0): # Stave only
-    ctx.stroke_segment(850, 200, 850, 350)
-    ctx.stroke_segment(840, 350, 1160, 350)
-    ctx.stroke_segment(1150, 350, 1150, 200)
+def halfRec(ctx, rotation=0): # Stave only
+    y = 200
+    if rotation == 0:
+        y2 = y + 150
+    else:
+        y = 2000 - y
+        y2 = y - 150
+    ctx.stroke_segment(850, y, 850, y2)
+    ctx.stroke_segment(840, y2, 1160, y2)
+    ctx.stroke_segment(1150, y2, 1150, y)
 
-def smallHalfRec(ctx, y, flip=0): # Stave only?
-    ctx.stroke_segment(850, 200, 850, 325)
-    ctx.stroke_segment(840, 325, 1160, 325)
-    ctx.stroke_segment(1150, 325, 1150, 200)
+def smallHalfRec(ctx, rotation=0): # Stave only?
+    y = 200
+    if rotation == 0:
+        y2 = y + 125
+    else:
+        y = 2000 - y
+        y2 = y - 125
+    ctx.stroke_segment(850, y, 850, y2)
+    ctx.stroke_segment(840, y2, 1160, y2)
+    ctx.stroke_segment(1150, y2, 1150, y)
 
-def mainLine(ctx, x, y):
-    ctx.stroke_segment(x, y, 1000, 1000)
-
+def mainLine(ctx, full=0):
+    if full == 0:
+        ctx.stroke_segment(1000, 200, 1000, 1000)
+    elif full in [1,3]:
+        ctx.stroke_segment(1000, 200, 1000, 1800)
+    elif full in [2,4]:
+        ctx.stroke_segment(200, 1000, 1800, 1000)
+    elif full == 3:
+# TODO Figure out how long diagonals need to be
+        ctx.stroke_segment(1000, 200, 1000, 1000)
+    
 def line(ctx, pos=0, rotate=0):
 # Unlike many other functions, this one only needs a 1 if it is a vertical line
 # 60 past the top of the section
-    if rotate == 1:
+    if rotate in [1,3]:
+        ctx.stroke_segment(910, 2000-pos, 1090, 2000-pos)
+    if rotate == 2:
         ctx.stroke_segment(pos, 910, pos, 1090)
     else: 
         ctx.stroke_segment(910, pos, 1090, pos)
 
-def sidewaysX(ctx, x, y, flip=0):
-    ctx.stroke_segment(920, 290, 1080, 220)
-    ctx.stroke_segment(920, 220, 1080, 290)
+def sidewaysX(ctx, y, rotation=0):
+# Pass in the lower value of y. Usually around 20 more than base
+    if rotation == 0:
+        y2 = y + 70
+    else:
+        y = 2000 - y
+        y2 = y - 70
+    ctx.stroke_segment(920, y, 1080, y2)
+    ctx.stroke_segment(920, y2, 1080, y)
 
-def pointUp(ctx, x, y, flip=0):
-    ctx.stroke_segment(925, 280, 1006, 195)
-    ctx.stroke_segment(994, 195, 1075, 280)
+def pointUp(ctx, y, rotation=0):
+# Pass in lower value. Usually is 5 less than base
+    if rotation == 0:
+        y2 = y + 85
+    else:
+         y = 2000 - y
+         y2 = y - 85
+    ctx.stroke_segment(925, y2, 1006, y)
+    ctx.stroke_segment(994, y, 1075, y2)
 
-def pointDown(ctx, x, y, flip=0):
-    ctx.stroke_segment(925, 210, 1006, 295)
-    ctx.stroke_segment(994, 295, 1075, 210)
+def pointDown(ctx, y, rotation=0):
+# Pass in lower value. Usually is 10 more than base
+    if rotation == 0:
+        y2 = y + 85
+    else:
+         y = 2000 - y
+         y2 = y - 85
+    ctx.stroke_segment(925, y, 1006, y2)
+    ctx.stroke_segment(994, y2, 1075, y)
 
-def curveDown(image, x, y, flip=0):
+def curveDown(image, y, rotation=0):
+# Usually y is 75 more than base
+    if rotation in [1,3]:
+        y = 2000 - y
+        rotation = 0
+    else:
+        rotation = 1
     path = pixie.parse_path(
-    """
-    M 925 275
-    A 75 75 90 0 1 1075 275
+    f"""
+    M 925 {y}
+    A 75 75 90 0 1 1075 {y}
     """
     )
     image.stroke_path(path, paint, stroke_width=20)
 
-def lineCirc(image, x, y, flip=0):
+"TODO make a flag that accounts for moving on the x axis for all circles"
+
+def lineCirc(image, y, rotation=0):
+# y is usually 50 from base?
+    if rotation in [1,3]:
+        y = 2000 - y
     path = pixie.parse_path(
-    """
-    M 950 250
-    A 50 50 90 0 1 1050 250
-    A 50 50 90 0 1 950 250
+    f"""
+    M 950 {y}
+    A 50 50 90 0 1 1050 {y}
+    A 50 50 90 0 1 950 {y}
     """
     )
     image.stroke_path(path, paint, stroke_width=20)
 
-def halfArrowR(ctx, x, y, flip=0):
-    ctx.stroke_segment(1005, 205, 1050, 310)
+def halfArrowR(ctx, y, rotation=0):
+# Pass in lower value. Usually is 5 more than base
+    if rotation == 0:
+        x, x2 = 1005, 1050
+        y2 = y + 105
+    else:
+        x, x2 = 995, 950
+        y = 2000 - y
+        y2 = y - 105
+    ctx.stroke_segment(x, y, x2, y2)
 
-def keyblade(ctx, x, y, flip=0):
-    ctx.stroke_segment(1000, 210, 1080, 210)
-    ctx.stroke_segment(1000, 240, 1080, 240)
+def keyblade(ctx, y, rotation=0):
+# Pass in lower value. Usually is 10 more than base
+    if rotation == 0:
+        x = 1080
+        y2 = y + 30
+    else:
+        x = 920
+        y = 2000 - y
+        y2 = y - 30
+    ctx.stroke_segment(1000, y, x, y)
+    ctx.stroke_segment(1000, y2, x, y2)
 
-def equals(ctx, x, y, flip=0):
-    ctx.stroke_segment(925, 240, 1075, 240)
-    ctx.stroke_segment(925, 270, 1075, 270)
+def equals(ctx, y, rotation=0):
+# Pass in lower value. Usually is 40 more than base
+    if rotation == 0:
+        y2 = y + 30
+    else:
+        y = 2000 - y
+        y2 = y - 30
+    ctx.stroke_segment(925, y, 1075, y2)
+    ctx.stroke_segment(925, y2, 1075, y)
 
-def flag(image, x, y, flip=0):
+def flag(image, y, rotation=0):
+    x = 1000
+    if rotation in [1,3]:
+        y2, y3, x2 = y - 50, y - 100, 940
+    else:
+        y2, y3, x2 = y + 50, y + 100, 1060
+
     path = pixie.parse_path(
-    """
-    M 1000 220
-    L 1060 270
-    L 1000 320
+    f"""
+    M {x} {y}
+    L {x2} {y2}
+    L {x} {y3}
     """
     )
     image.stroke_path(path, paint, stroke_width=20)
 
-def diamond(image, x, y, flip=0):
+def diamond(image, y, rotation=0):
+    x, x2, x3 = 1000, 950, 1050
+    if rotation in [1,3]:
+        y2, y3 = y - 50, y - 100
+    else:
+        y2, y3 = y + 50, y + 100
     path = pixie.parse_path(
-    """
-    M 1000 200
-    L 1050 250
-    L 1000 300
-    L 950 250
+    f"""
+    M {x} {y}
+    L {x3} {y2}
+    L {x} {y3}
+    L {x2} {y2}
     Z
     """
     )
     image.stroke_path(path, paint, stroke_width=20)
 
-def antlers(ctx, x, y, flip=0): # Stave only
-    ctx.stroke_segment(890, 250, 1110, 250)
-    ctx.stroke_segment(900, 250, 900, 200)
-    ctx.stroke_segment(945, 250, 945, 200)
-    ctx.stroke_segment(1055, 250, 1055, 200)
-    ctx.stroke_segment(1100, 250, 1100, 200)
+def antlers(ctx, rotation=0): # Stave only
+    if rotation == 0:
+        y, y2 = 200, y + 50
+    else:
+        y = 1800
+        y2 = y - 50
+    ctx.stroke_segment(890, y2, 1110, y2)
+    ctx.stroke_segment(900, y2, 900, y)
+    ctx.stroke_segment(945, y2, 945, y)
+    ctx.stroke_segment(1055, y2, 1055, y)
+    ctx.stroke_segment(1100, y2, 1100, y)
 
-def smallLineCirc(image, x, y, flip=0):
+def smallLineCirc(image, y, rotation=0):
+# y is usually 40 from base
+    if rotation in [1,3]:
+        y = 2000 - y
+
     path = pixie.parse_path(
-    """
-    M 960 240
-    A 30 30 90 0 1 1040 240
-    A 30 30 90 0 1 960 240
+    f"""
+    M 960 {y}
+    A 30 30 90 0 1 1040 {y}
+    A 30 30 90 0 1 960 {y}
     """
     )
     image.stroke_path(path, paint, stroke_width=20)
 
-def smallFillCirc(image, x, y, flip=0):
+def smallFillCirc(image, y, rotation=0):
+# y is usually 40 from base
+    if rotation in [1,3]:
+        y = 2000 - y
+
     path = pixie.parse_path(
-    """
-    M 965 240
-    A 20 20 90 0 1 1035 240
-    A 20 20 90 0 1 965 240
+    f"""
+    M 965 {y}
+    A 20 20 90 0 1 1035 {y}
+    A 20 20 90 0 1 965 {y}
     """
     )
 
     image.fill_path(path, fill)
     smallLineCirc(image)
 
-def doubleDots(image, x, y, flip=0):
+def doubleDots(image, y, rotation=0):
+# y is usually 100 from base
+    if rotation in [1,3]:
+        y = 2000 - y
     path = pixie.parse_path(
-    """
-    M 940 300
-    A 10 10 90 0 1 960 300
-    A 10 10 90 0 1 940 300
-    M 1040 300
-    A 10 10 90 0 1 1060 300
-    A 10 10 90 0 1 1040 300
+    f"""
+    M 940 {y}
+    A 10 10 90 0 1 960 {y}
+    A 10 10 90 0 1 940 {y}
+    M 1040 {y}
+    A 10 10 90 0 1 1060 {y}
+    A 10 10 90 0 1 1040 {y}
     """
     )
     image.fill_path(path, paint)
 
-def shieldedOrb(image, x, y, flip=0):
+def shieldedOrb(image, y, rotation=0):
+# y is usually 50 from base
+    if rotation in [1,3]:
+        y = 2000 - y
     path = pixie.parse_path(
-    """
-    M 945 250
-    A 55 55 90 0 1 1055 250
-    A 55 55 90 0 1 945 250
+    f"""
+    M 945 {y}
+    A 55 55 90 0 1 1055 {y}
+    A 55 55 90 0 1 945 {y}
     """
     )
 
@@ -186,15 +301,31 @@ def shieldedOrb(image, x, y, flip=0):
     )
     image.fill_path(path, paint)
 
-def strSave(ctx, x, y):
-    ctx.stroke_rect(900, 900, 200, 200)
-    ctx.stroke_rect(950, 950, 100, 100)
+def strSave(ctx, y):
+    y2 = y + 50
+    ctx.stroke_rect(900, y, 200, 200)
+    ctx.stroke_rect(950, y2, 100, 100)
 
 class Identity:
     def __init__(spell, name):
         spell.name = name
-        spell.x = 1000 # x and y are more like the center of where to put any symbol
-        spell.y
+    
+    def collected(spell, functions, args, rotations, ctx, image):
+        for rotation in range(0,rotations+1):
+            if rotation == 3:
+                cx, cy = image.width / 2, image.height / 2
+                ctx.translate(cx, cy)
+                ctx.rotate(angle_radians)
+                ctx.translate(-cx, -cy)
+                mainLine(ctx,1)
+                mainLine(ctx,2)
+            for func, arg in zip(functions, args):
+                func(*arg, rotation)
+            if rotation == 4:
+                ctx.translate(cx, cy)
+                ctx.rotate(-angle_radians)
+                ctx.translate(-cx, -cy)
+         
 
 class School(Identity):
     def __init__(spell, name):
@@ -206,7 +337,9 @@ class School(Identity):
         else:
             spell.damage = 0
 
-    def draw(spell, ctx, image):
+    def draw(spell, ctx, image, rotation):
+        functions = []
+        args = []
         if spell.damage == 1:
             if spell.name in ["Bludgeoning", "Piercing", "Slashing"]:
                 halfRec(ctx)
@@ -220,24 +353,28 @@ class School(Identity):
                 smallHalfRec(ctx)
                 equals(ctx)
             
-            elif spell.name == "Fire":
-                pass
-            elif spell.name == "Cold":
-                pass
-            elif spell.name == "Lightning":
-                pass
-            elif spell.name == "Thunder":
-                pass
-            elif spell.name == "Acid":
-                pass
-            elif spell.name == "Poison":
-                pass
-            elif spell.name == "Necrotic":
-                pass
-            elif spell.name == "Radiant":
-                pass
-            elif spell.name == "Psychic":
-                pass
+            else:
+                functions.append(bigCurve)
+                args.append([image])
+                if spell.name == "Fire":
+                    pass
+                elif spell.name == "Cold":
+                    pass
+                elif spell.name == "Lightning":
+                    pass
+                elif spell.name == "Thunder":
+                    pass
+                elif spell.name == "Acid":
+                    functions.append(keyblade)
+                    args.append([ctx,spell.y+10])
+                elif spell.name == "Poison":
+                    pass
+                elif spell.name == "Necrotic":
+                    pass
+                elif spell.name == "Radiant":
+                    pass
+                elif spell.name == "Psychic":
+                    pass
         elif spell.name == "Buff":
                 pass
         elif spell.name == "Debuff":
@@ -280,13 +417,16 @@ class School(Identity):
                 pass
         elif spell.name == "Utility":
                 pass
+        super().collected(functions,args, rotation, ctx, image)
         
 class Range(Identity):
     def __init__(spell, name):
           super().__init__(name)
           spell.y = 500
 
-    def draw(spell, ctx, image):
+    def draw(spell, ctx, image, rotation):
+        functions = []
+        args = []
         if spell.name == "Self":
             pass
         elif spell.name == "Touch":
@@ -298,7 +438,8 @@ class Range(Identity):
         elif spell.name == "30 Feet":
             pass
         elif spell.name == "60 Feet":
-            pass
+            functions.append(smallCurve)
+            args.append([image, spell.y])
         elif spell.name == "90 Feet":
             pass
         elif spell.name == "100 Feet":
@@ -315,15 +456,19 @@ class Range(Identity):
             pass
         elif spell.name == "Sight":
             pass
+        super().collected(functions,args, rotation, ctx, image)
 
 class Duration(Identity):
     def __init__(spell, name):
           super().__init__(name)
           spell.y = 700
 
-    def draw(spell, ctx, image):
+    def draw(spell, ctx, image, rotation):
+        functions = []
+        args = []
         if spell.name == "Instant":
-            pass
+            functions.append(line)
+            args.append([ctx,spell.y])
         elif spell.name == "1 Round":
             pass
         elif spell.name == "1 Minute":
@@ -354,12 +499,20 @@ class Duration(Identity):
             pass
         elif spell.name == "30 Days":
             pass
+        super().collected(functions,args, rotation, ctx, image)
 
 class Save(Identity):
-    def __init__(spell, name):
-          super().__init__(name)
+    def __init__(spell, name, cantrip):
+            super().__init__(name)
+            if cantrip == 1:
+                spell.y = 800
+            else:
+                spell.y = 900
+
 
     def draw(spell, ctx, image):
+        functions = []
+        args = []
         if spell.name == "None":
             return
         elif spell.name == "Dexterity":
@@ -369,28 +522,14 @@ class Save(Identity):
         elif spell.name == "Wisdom":
             pass
         elif spell.name == "Strength":
-            pass
+            strSave(ctx,spell.y)
         elif spell.name == "Charisma":
             pass
         elif spell.name == "Intelligence":
             pass
          
 
-def main():
-    image = pixie.Image(2000,2000)
-    image.fill(pixie.Color(1, 1, 1, 1))
 
-    ctx = image.new_context()
-    ctx.stroke_style = paint
-    ctx.line_width = 25
-    mainLine(ctx, 1000, 200)
-
-
-    ctx.line_width = 20
-
-
-
-    image.write_file("trying.png")
 
 
     # Sideways
@@ -409,8 +548,3 @@ def main():
     # ctx.stroke_segment(1500, 905, 1500, 1095)
     # ctx.stroke_segment(1300, 905, 1300, 1095)
     # image.stroke_path(spath, paint, stroke_width=20)
-
-
-
-if __name__ == "__main__":
-    main()
--- NordicSpellCrafting/Nordify.py
+++ NordicSpellCrafting/Nordify.py
@@ -1,15 +1,69 @@
+from Classifications import School, Range, Duration, Save, mainLine
+import pixie
 
+paint = pixie.Paint(pixie.SOLID_PAINT)
+paint.color = pixie.parse_color("#000000")
+
+fill = pixie.Paint(pixie.SOLID_PAINT)
+fill.color = pixie.parse_color("#FFFFFF")
 
 class Spell:
-    def __init__(spell, comp1, comp2, comp3, comp4):
-        spell.name = comp1
-        spell.school = comp2
-        spell.time = comp3
-        spell.blurb = comp4
+    def __init__(spell, name, tag, dist, dur, save, lvl):
+        spell.name = name
+        spell.school = School(tag)
+        spell.range = Range(dist)
+        spell.duration = Duration(dur)
+        if lvl == 0:
+            spell.save = Save(save, 1)
+        else:
+            spell.save = Save(save, 0)
+        spell.level = int(lvl)
+        spell.drawSpell()
 
-    def deets(spell):
-        print(f"You want to know more about {spell.name}?\nWell it is in the school of {spell.school} and it takes about a {spell.time} to cast!\nHere is some more info if you are curious\n{spell.blurb}")
+    
 
+    def drawSpell(spell):
+        image = pixie.Image(2000,2000)
+        image.fill(pixie.Color(1, 1, 1, 1))
+
+        ctx = image.new_context()
+        ctx.stroke_style = paint
+        ctx.line_width = 25
+        # TODO So this would probably be best to get thrown into a try except block
+        if spell.level == 0:
+            mainLine(ctx, 0)
+            ctx.line_width = 20
+            spell.school.draw(ctx,image,0)
+            spell.range.draw(ctx,image,0)
+            spell.duration.draw(ctx,image,0)
+            spell.save.draw(ctx,image)
+
+        else:
+            mainLine(ctx, 1)
+            if spell.level == 1:
+                ctx.line_width = 20
+                spell.school.draw(ctx,image,1)
+                spell.range.draw(ctx,image,1)
+                spell.duration.draw(ctx,image,1)
+                spell.save.draw(ctx,image)
+            elif spell.level in [2,4,7]:
+                mainLine(ctx,2)
+                ctx.line_width = 20
+                spell.school.draw(ctx,image,2)
+                spell.range.draw(ctx,image,2)
+                spell.duration.draw(ctx,image,2)
+                spell.save.draw(ctx,image)
+            else:
+                mainLine(ctx,2)
+                ctx.line_width = 20
+                spell.school.draw(ctx,image,4)
+                spell.range.draw(ctx,image,4)
+                spell.duration.draw(ctx,image,4)
+                spell.save.draw(ctx,image)
+
+        image.write_file(f"NordifiedSpells/{spell.name}.png")
+
+        
+        
 if __name__ == "__main__":
-    test = Spell("eldritch blast", "evocation","reaction", "You think you need to know more punk?" )  
-    test.deets()
+    Spell('Acid Splash', 'DMG Acid', '60 Feet', 'Instant', 'Strength', 3)
